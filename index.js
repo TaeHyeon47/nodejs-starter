b421e047f3736aea1cd8c8d8487b8acd761a15e3
TaeHyeon <hillskuti@naver.com>
2024-05-11 13:39:44 +0900

    nodejs 모듈 설치 및 URL 설정

--- index.js
+++ index.js
@@ -1,86 +1,93 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 
-/////////////////////
-// FilE
+/////////////////////////////////
+// FILES
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 // console.log(textIn);
-
 // const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 // fs.writeFileSync('./txt/output.txt', textOut);
 // console.log('File written!');
 
 // Non-blocking, asynchronous way
 // fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR! 💥');
+
 //   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
 //     console.log(data2);
-//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
 //       console.log(data3);
-//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-//         console.log('Your file has been written !');
-//         console.log('git text')
-//       });
+
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written 😁');
+//       })
 //     });
 //   });
 // });
 // console.log('Will read file!');
 
-//////////////////////////
+/////////////////////////////////
 // SERVER
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = temp.replace(/{%IMAGE%}/g, product.image);
-};
-
-// 프로그램이 실행될 때 생성되므로 API 요청 시, 코드 실행을 막지 않음.
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  `utf-8`
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  `utf-8`
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  `utf-8`
+  'utf-8'
 );
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObject = JSON.parse(data);
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true);
 
   // Overview page
-  if (pathName === '/' || pathName === '/overview') {
-    res.writeHead(200, { 'Content-type': 'text/html' });
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
 
-    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el));
-
-    res.end(tempOverview);
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
 
     // Product page
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT');
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-  } else if (pathName === '/api') {
-    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, { 'Content-type': 'application/json' });
-      res.end(data);
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
     });
+    res.end(data);
 
     // Not found
   } else {
     res.writeHead(404, {
-      'Content-Type': 'text/html',
-      'my-own-header': 'hello-world',
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
     });
-    res.end('<h1>Page not found</h1>');
+    res.end('<h1>Page not found!</h1>');
   }
 });
 
